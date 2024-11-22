17b6645de4685f74b6dea48231bfad34e1fcf7b7
Amanda Cardoso <61628738+Amndc@users.noreply.github.com>
2024-11-22 13:03:27 -0300

    Update ColorUtils.js

--- ColorUtils.js
+++ ColorUtils.js
@@ -1,3 +1,4 @@
+//Gera cores monocromaticas apartir da cor desejada em rgb. 
 function CriaCoresMonocromaticoRGB(color) {
             var rgbValues = color.match(/\d+/g).map(Number);
             var vermelho = rgbValues[0];
@@ -12,3 +13,20 @@ function CriaCoresMonocromaticoRGB(color) {
 
             return `${newVermelo} ${newVerde} ${newAzul}`;
     }
+
+// Valida Luminosidade de uma cor.
+function validaLuminosidade(hex) {
+    var nib = hex.split('');
+
+    var r = parseInt(nib[1] + nib[2], 16);
+    var g = parseInt(nib[3] + nib[4], 16);
+    var b = parseInt(nib[5] + nib[6], 16);
+    var lum = (r * 299 + g * 587 + b * 114) / 1000;
+
+    if (lum > 128) {
+       return 1
+    }
+    else {
+       return 0
+    }
+}
